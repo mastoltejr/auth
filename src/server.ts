fe8abc67248485434fdc0a182fcad6e3b4346925
Michael Stolte <michael.stoltejr@gmail.com>
2022-01-24 22:52:02 -0700

    Removed Async, worked on forms

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,32 @@
 import express, { Application, Request, Response, NextFunction } from 'express';
 import cors from 'cors';
-import redis from 'redis';
-import { createConnection, MysqlError } from 'mysql';
+import { createClient, RedisClient } from 'redis';
+import {
+	createConnection,
+	MysqlError,
+	Connection as MysqlConnection
+} from 'mysql';
 import { genSalt, hash, compare } from 'bcrypt';
+import { promisify } from 'util';
 import dotenv from 'dotenv';
+import { join } from 'path';
 dotenv.config();
 
-const redisClient = redis.createClient(6379, process.env['REDIS_HOST'], {
+let redis: RedisClient;
+let mysql: MysqlConnection;
+
+redis = createClient(6379, process.env['REDIS_HOST'], {
 	password: process.env['REDIS_PWD'],
 	db: process.env['REDIS_DB']
 });
 
-redisClient.on('connection', () => console.log('RedisClient connected'));
-redisClient.on('error', (err) => console.log('RedisClient error', err));
+redis.on('connection', () => console.log('Redis is connected'));
+redis.on('error', (err) => {
+	console.log('Redis errored out', err);
+	process.exit();
+});
 
-var connection = createConnection({
+mysql = createConnection({
 	host: String(process.env['MYSQL_HOST']),
 	port: +String(process.env['MYSQL_PORT']),
 	database: String(process.env['MYSQL_DB']),
@@ -22,49 +34,103 @@ var connection = createConnection({
 	password: String(process.env['MYSQL_PASSWORD'])
 });
 
-connection.connect((err: MysqlError) =>
-	err
-		? console.log('error connecting: ' + err.stack)
-		: console.log('connected to mysql as id ' + connection.threadId)
-);
+mysql.connect((err: MysqlError) => {
+	if (err) {
+		console.log('Mysql errored out', err);
+		process.exit();
+	} else {
+		console.log('Mysql is connected');
+	}
+});
 
-connection.query(
-	`SELECT 
-		* 
-	FROM information_schema.tables 
-	WHERE 
-		table_schema = '${String(process.env['MYSQL_DB'])}' AND 
-		table_name = 'AUTH_USERS' 
-	LIMIT 1;`,
-	(error, results, fields) => {
-		if (error) throw error;
-		if ((results as any[]).length === 0) {
-			connection.query(
-				`CREATE TABLE auth.AUTH_USERS(
-					id INT NOT NULL AUTO_INCREMENT,
-					username VARCHAR(256) NOT NULL,
-					password BINARY(60) NOT NULL,
-					firstName VARCHAR(35),
-					lastName VARCHAR(35),
-					email VARCHAR(256) NOT NULL,
-					birthday DATE,
-					creationDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
-					lastAccessDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-					active BOOLEAN DEFAULT TRUE,
-					PRIMARY KEY (id)
-				)`,
-				(err, _, __) => {
-					if (err) throw err;
-				}
-			);
+mysql.query(
+	"SELECT * FROM information_schema.tables WHERE table_schema = ? AND table_name = 'AUTH_USERS'",
+	[String(process.env['MYSQL_DB'])],
+	(_, results, __) => {
+		if (results.length === 0) {
+			mysql.query(`CREATE TABLE auth.AUTH_USERS(
+							id INT NOT NULL AUTO_INCREMENT,
+							username VARCHAR(256) NOT NULL,
+							password BINARY(60) NOT NULL,
+							firstName VARCHAR(35),
+							lastName VARCHAR(35),
+							email VARCHAR(256) NOT NULL,
+							birthday DATE,
+							creationDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP,
+							lastAccessDate TIMESTAMP NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+							active BOOLEAN DEFAULT TRUE,
+							PRIMARY KEY (id)
+						)`);
 		} else {
-			console.log('Auth user table exists');
+			console.log('Mysql: AUTH_USERS exists');
 		}
 	}
 );
 
+const asyncMiddleware =
+	(fn: (req: Request, res: Response, next: NextFunction) => Promise<any>) =>
+	(req: Request, res: Response, next: NextFunction) => {
+		Promise.resolve(fn(req, res, next)).catch(next);
+	};
+
+let query = promisify(mysql.query).bind(mysql);
 const app: Application = express();
+
 app.use(express.json());
+app.set('view engine', 'ejs');
+app.set('views', join(__dirname, '/pages'));
+
+app.put(
+	'/user/register',
+	cors,
+	asyncMiddleware(async (req, res, next) => {
+		const { username, password, email } = req.body;
+		const body = req.body;
+
+		if (!(username && password && email)) {
+			return res.status(400).send({ error: 'Data not formatted properly' });
+		}
+
+		let results = await query({
+			sql: 'SELECT ID FROM AUTH_USERS WHERE username = :username',
+			values: {
+				username,
+				email
+			}
+		});
+		if ((results as any[]).length > 0) {
+			return res.status(400).send({ error: 'Non-unique username' });
+		}
+
+		results = await query({
+			sql: 'SELECT ID FROM AUTH_USERS WHERE email = :email',
+			values: {
+				username,
+				email
+			}
+		});
+		if ((results as any[]).length > 0) {
+			return res.status(400).send({ error: 'Non-unique email' });
+		}
+
+		// creating a new mongoose doc from user data
+		//const user = new User(body);
+		// generate salt to hash password
+		//const salt = await bcrypt.genSalt(10);
+		// now we set user password to hashed password
+		//user.password = await bcrypt.hash(user.password, salt);
+		//user.save().then((doc) => res.status(201).send(doc));
+	})
+);
+
+app.get('/', (req, res) => res.render('home'));
+app.get('/login', (req, res) => res.render('login'));
+app.get('/signup', (req, res) => res.render('signup'));
+
+const expressPort = process.env.EXPRESS_PORT || 5000;
+app.listen(expressPort, () =>
+	console.log(`Server running on port ${expressPort}`)
+);
 
 /*
 1) Sign In User
@@ -85,16 +151,15 @@ app.use(express.json());
 	
 	Username
 	Password
-	First Name
-	Last Name
-	Birthday
+	First Name?
+	Last Name?
+	Birthday?
 	Email
-	Phone Number
-	Address (Street 1, Street 2, City, State, Zip Code)
+	Phone Number?
+	Address? (Street 1, Street 2, City, State, Zip Code)
 
 	Store this information in MYSQL database
 */
-app.put('/user/register', cors, (req, res) => {});
 
 /*
 3) Update User
@@ -183,8 +248,3 @@ app.get('/:key', (req: Request, res: Response, next: NextFunction) => {
   });
 
 */
-
-const expressPort = process.env.EXPRESS_PORT || 5000;
-app.listen(expressPort, () =>
-	console.log(`Server running on port ${expressPort}`)
-);
